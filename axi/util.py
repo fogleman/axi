cc4551e990713aa28fd6a3e10634b825041f6e3e
Sam Gentle <sam@samgentle.com>
2018-03-08 22:30:37 +1100

    make util always clean up after drawing

--- axi/util.py
+++ axi/util.py
@@ -9,5 +9,8 @@ def draw(drawing, progress=True):
     # TODO: support drawing, list of paths, or single path
     d = Device()
     d.enable_motors()
-    d.run_drawing(drawing, progress)
-    d.disable_motors()
+    try:
+        d.run_drawing(drawing, progress)
+    finally:
+        d.disable_motors()
+        d.pen_up()
