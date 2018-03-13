81c225eb2e5653f580ccc8d7c7d65b9d6176fca7
Michael Fogleman <fogleman@gmail.com>
2018-03-13 10:53:50 -0400

    angles

---
+++ examples/angles.py
@@ -0,0 +1,27 @@
+from __future__ import division, print_function
+
+import axi
+import math
+import random
+import sys
+
+BOUNDS = axi.V3_BOUNDS
+RADIUS = 4
+STEP = 5
+
+def main():
+    w, h = BOUNDS[-2:]
+    paths = []
+    r = RADIUS
+    for a in range(0, 180, STEP):
+        a = math.radians(a)
+        c = math.cos(a)
+        s = math.sin(a)
+        paths.append([(-r * c, -r * s), (r * c, r * s)])
+    d = axi.Drawing(paths)
+    d = d.center(w, h)
+    d.dump('out.axi')
+    d.render(bounds=BOUNDS).write_to_png('out.png')
+
+if __name__ == '__main__':
+    main()
